e972fcb8ced9956bce510fe7ccc27a8f6d454881
kyleabeauchamp <kyleabeauchamp@gmail.com>
2014-11-07 19:02:29 -0500

    Added some stuff

--- src/plot_virtualchemistry.py
+++ src/plot_virtualchemistry.py
@@ -11,11 +11,11 @@ metadata = pd.read_table("./virtualchemistry.tab").set_index("cas")
 data = pd.read_csv("./vchem.csv").set_index("cas")
 data["formula"] = metadata.formula
 data["polcorr"] = pd.Series(dict((cas, polarizability.dielectric_correction_from_formula(formula, density * u.grams / u.milliliter)) for cas, (formula, density) in data[["formula", "density"]].iterrows()))
-data["corrected"] = data.expt - data.polcorr
+data["corrected"] = data.gaff + data.polcorr
 data["gaffdiff"] = data.expt - data.gaff
-#result = sm.ols(formula="gaffdiff ~ polcorr", data=data).fit()
-data["optcorr"] = -1.369696 + 4.626496 * data.polcorr
-data["optcorrected"] = data.expt - data.optcorr
+result = sm.ols(formula="gaffdiff ~ polcorr", data=data[data.expt < 6]).fit()
+data["optcorr"] = result.params.Intercept + result.params.polcorr * data.polcorr
+data["optcorrected"] = data.gaff + data.optcorr
 
 
 
@@ -25,16 +25,17 @@ g.set(xscale='log', xlim=(1, 100), ylim=(1, 100))
 g.set(yscale='log')
 
 
-g = sns.lmplot("gaff", "corrected", data, fit_reg=False)
+g = sns.lmplot("corrected", "expt", data, fit_reg=False)
 plt.plot([1, 100], [1, 100], 'k')  # Guide
 g.set(xscale='log', xlim=(1, 100), ylim=(1, 100))
 g.set(yscale='log')
 
-g = sns.lmplot("gaff", "optcorrected", data, fit_reg=False)
+g = sns.lmplot("optcorrected", "expt", data, fit_reg=False)
 plt.plot([1, 100], [1, 100], 'k')  # Guide
 g.set(xscale='log', xlim=(1, 100), ylim=(1, 100))
 g.set(yscale='log')
 
+"""
 figure()
 g = sns.residplot("gaff", "expt", data)
 g.set(xscale='log', xlim=(1, 100))
@@ -48,8 +49,4 @@ g.set(xscale='log', xlim=(1, 100))
 figure()
 g = sns.residplot("gaff", "optcorrected", data)
 g.set(xscale='log', xlim=(1, 100))
-
-
-
-
-result = sm.ols(formula="gaffdiff ~ polcorr", data=data).fit()
+"""
