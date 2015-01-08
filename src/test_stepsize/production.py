e9b02267bf14d11b53726b3a9f76282c3aad58a2
kyleabeauchamp <kyleabeauchamp@gmail.com>
2015-01-08 18:01:40 -0500

    Some fixes

--- src/test_stepsize/production.py
+++ src/test_stepsize/production.py
@@ -4,18 +4,19 @@ from simtk import unit as u
 
 cas = "126492-54-4"
 
-timestep_factor = 1.
+timestep_factor = 2.
 
-base_filename = "langevin%0.3fs" % 2.0 / timestep_factor
+base_filename = "langevin%0.1fs" % (2.0 / timestep_factor)
 
-output_frequency = 500 * timestep_factor
-dcd_frequency = output_frequency * 20
+output_frequency = int(500 * timestep_factor)
+dcd_frequency = int(output_frequency * 20)
+barostat_frequency = int(25 * timestep_factor)
+n_steps = int(500000000 * timestep_factor)
 
 friction = 1.0 / u.picoseconds
 temperature = 300. * u.kelvin
 pressure = 1.0 * u.atmospheres
-barostat_frequency = 25
-n_steps = 500000000
+
 cutoff = 1.0 * u.nanometers
 
 ffxml_filename = "%s.xml" % cas
@@ -31,7 +32,7 @@ positions = pdb.positions
 
 system = ff.createSystem(topology, nonbondedMethod=app.PME, nonbondedCutoff=cutoff, constraints=app.HBonds)
 
-integrator mm.LangevinIntegrator(temperature, friction, 2.0 * u.femtoseconds / timestep_factor)
+integrator = mm.LangevinIntegrator(temperature, friction, 2.0 * u.femtoseconds / timestep_factor)
 
 system.addForce(mm.MonteCarloBarostat(pressure, temperature, barostat_frequency))
 
@@ -42,5 +43,4 @@ simulation.context.setVelocitiesToTemperature(temperature)
 
 simulation.reporters.append(app.DCDReporter(dcd_filename, dcd_frequency))
 simulation.reporters.append(app.StateDataReporter(open(log_filename, 'w'), output_frequency, step=True, time=True, speed=True, density=True))
-print(integrator_type, output_frequency)
 simulation.step(n_steps)
